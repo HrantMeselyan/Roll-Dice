546bb0316b50cc28adc37af2cd3f969f6c965257
dell <hrant3307@gmail.com>
2023-09-04 15:16:17 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,132 @@
+'use strict';
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+init();
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
+
+function roll() {
+    if (playing) {
+        const dice = Math.trunc(Math.random() * 6) + 1;
+
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
+
+        if (dice !== 1) {
+            currentScore += dice;
+            document.getElementById(
+                `current--${activePlayer}`
+            ).textContent = currentScore;
+        } else {
+            switchPlayer();
+        }
+    }
+}
+
+function hold() {
+
+    if (playing) {
+        scores[activePlayer] += currentScore;
+
+        document.getElementById(`score--${activePlayer}`).textContent =
+            scores[activePlayer];
+
+        if (scores[activePlayer] >= 100) {
+            playing = false;
+            diceEl.classList.add('hidden');
+
+            document
+                .querySelector(`.player--${activePlayer}`)
+                .classList.add('player--winner');
+            document
+                .querySelector(`.player--${activePlayer}`)
+                .classList.remove('player--active');
+            toastr.options = {
+                "closeButton": false,
+                "debug": false,
+                "newestOnTop": false,
+                "progressBar": false,
+                "positionClass": "toast-top-center",
+                "preventDuplicates": false,
+                "onclick": null,
+                "showDuration": "300",
+                "hideDuration": "1000",
+                "timeOut": "5000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            }
+            toastr["success"](`Player ${activePlayer + 1} Won`)
+        } else {
+            switchPlayer();
+        }
+    }
+}
+
+btnNew.addEventListener('click', init);
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'a' && activePlayer === 0) {
+        roll();
+    }
+});
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'l' && activePlayer === 1) {
+        roll();
+    }
+});
+document.addEventListener('keydown', function (e) {
+    if (e.key === 's' && activePlayer === 0) {
+        hold();
+    }
+});
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'k' && activePlayer === 1) {
+        hold();
+    }
+});
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+        init();
+    }
+});
+
+btnRoll.addEventListener('click', roll);
+btnHold.addEventListener('click', hold);
